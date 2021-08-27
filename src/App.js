85993f55f2b05ad530b0255b30ac8063e6f3983b
felix <felixfranko66@gmail.com>
2021-08-27 03:29:25 +0300

    add task and taskitem

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
+import React, { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Study React Pre-Class Notes',
+      day: 'Feb 5th at 2:30pm',
+      isDone: false,
+    },
+    {
+      id: 2,
+      text: 'Feed the Dog',
+      day: 'Feb 6th at 1:30pm',
+      isDone: false,
+    },
+    {
+      id: 3,
+      text: 'Attend in-Class',
+      day: 'Feb 7th at 20:00pm',
+      isDone: false,
+    },
+  ]);
+
+  //CRUD
+  //Add Tasks
+
+  //Delete tasks
+  const deleteTask = (deleteTaskId) => {
+    console.log('delete', deleteTaskId);
+    setTasks(tasks.filter((task) => task.id !== deleteTaskId));
+  };
+
+  //Toggle Done
+
   return (
     <div className="container">
       <Header title="Task Tracker" />
-      <Tasks />
+      <Tasks tasks={tasks} deleteTask={deleteTask} />
     </div>
   );
 }
